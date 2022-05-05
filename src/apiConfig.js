ba4e071dcc34d2bcc477ca3dc9517e9bf8871f54
jrowla2 <jrowla2@gmail.com>
2022-05-05 09:27:41 -0600

    Correct tab errors

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
-	production: 'https://cryptic-brushlands-17541.herokuapp.com',
-	development: 'http://localhost:4741',
+  production: 'https://cryptic-brushlands-17541.herokuapp.com',
+  development: 'http://localhost:4741'
 }
 
 if (window.location.hostname === 'localhost') {
